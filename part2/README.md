198174e94e25062f8b49897dd46376dfe897137e
Joao Vicente <joao.diogo.vicente@gmail.com>
2018-03-07 22:51:26 +0000

    added Jaeger tracing for REST controller

--- part2/README.md
+++ part2/README.md
@@ -1,2 +1,20 @@
 # stories-event-driven-study-part-1
 This repo contains the code resulting from following the steps in [Event driven with Spring REST, Kafka and MongoDB - part 1](https://joaovicente.github.io/2018-02-22-event-driven-with-spring-rest-kafka-and-mongodb-part1/) blog
+
+Start Kafka and MongoDB, and run spring boot app
+```
+$ docker-compose -f ./docker-compose-kafka-mongo.yml up
+$ mvn spring-boot:run
+```
+
+Start Jaeger
+```
+ docker run -it --network=host jaegertracing/all-in-one
+```
+
+Make a call
+```
+$ http post localhost:8080/authors name=me email=me@gmail.com
+```
+
+Look at your trace in Jaeger http://localhost:16686
--- part2/stories/src/main/java/io/github/joaovicente/stories/StoriesApplication.java
+++ part2/stories/src/main/java/io/github/joaovicente/stories/StoriesApplication.java
@@ -1,10 +1,22 @@
 package io.github.joaovicente.stories;
 
+import com.uber.jaeger.samplers.ProbabilisticSampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.uber.jaeger.Configuration;
 
 @SpringBootApplication
 public class StoriesApplication {
+
+	@Bean
+	public io.opentracing.Tracer jaegerTracer() {
+		return new Configuration("spring-boot",
+				new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
+				new Configuration.ReporterConfiguration())
+				.getTracer();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoriesApplication.class, args);
